061a359a3dccb4856bd4a1386c5ea64479da0fdd
Morvay Balázs <balazsmorvay@yahoo.com>
2021-02-17 13:12:58 +0100

    initial commit

---
+++ SpotifySDK/SpotifySDK.h
@@ -0,0 +1,19 @@
+//
+//  SpotifySDK.h
+//  SpotifySDK
+//
+//  Created by Morvay Mac on 2021. 02. 17..
+//  Copyright © 2021. Morvay Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifySDK.
+FOUNDATION_EXPORT double SpotifySDKVersionNumber;
+
+//! Project version string for SpotifySDK.
+FOUNDATION_EXPORT const unsigned char SpotifySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifySDK/PublicHeader.h>
+
+
